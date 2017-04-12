39f68b04a9edfa37a0b8c973a732fa943a6b7784
Yannick Méheut <yannick@meheut.org>
2017-04-12 17:33:23 +0200

    Defined licence

--- mimipenguin.py
+++ mimipenguin.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
-# Copyright © 2017, Yannick Méheut (github.com/the-useless-one) <yannick@meheut.org>
+#   Rewrite of mimipenguin in Python 3.
+#   Original idea from Hunter Gregal (@huntergregal).
+#   Implementation by Yannick Méheut (github.com/the-useless-one)
+#   Copyright © 2017, Yannick Méheut <yannick@meheut.org>
+
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 import platform
